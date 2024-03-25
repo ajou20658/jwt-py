06f610eadc56a2b4e50be7a2658645d94a1de84e
ajou20658 <kwy1379@ajou.ac.kr>
2024-03-25 20:36:55 +0900

    불필요한 환경 변수 로컬 변수화

--- func.py
+++ func.py
@@ -7,7 +7,7 @@ import logging
 redis_client= redis.Redis(host='redis-logout',port=6379,db=0)
 EXPIRATION = 86400
 REFRESH = 604800
-SECRET_KEY = os.environ.get('JWT_SECRET',default="d342368781b90fdd52a18d49187fa56366b5ff805bbeec9836a3811311d60b210f6f9044c840bbe8d5febeceb08d10f5d870e2af15a965634d1ff4b387535092")
+SECRET_KEY = os.environ.get('JWT_SECRET')
 
 def blacklist_refresh_token(refresh,expiration_time):
     now = datetime.datetime.now().timestamp()
